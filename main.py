a2c844acd54c28674d0cba0f87c56e4b135b6f8f
ArdenchikPlus <raskovalov132@gmail.com>
2026-06-04 21:13:13 +0300

    adding a capitalization check

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 n=input()
 check1=0
 check2=False
+check3=False
 result=0
+num="123456789"
 for i in range(len(n)):
     check1=check1+1
     if n[i].isupper():
         check2=True
+    if n[i] in num:
+        check3=True
 if check1 < 8:
     print("Ошибка: в пароле меньше 8 символов")
 else:
@@ -14,6 +18,9 @@ if check2 != True:
     print("Ошибка: в пароле нет заглавной буквы")
 else:
     result=result+1
-
+if check3 == False:
+    print("Ошибка: в пароле нет цифр")
+else:
+    result=result+1
 
 
