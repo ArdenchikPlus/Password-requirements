56323c1e2fca43c8b54038518d87bc38d3d213a0
ArdenchikPlus <raskovalov132@gmail.com>
2026-06-04 21:00:20 +0300

    adding a character check

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 n=input()
 check1=0
+check2=False
 result=0
 for i in range(len(n)):
     check1=check1+1
+    if n[i].isupper():
+        check2=True
 if check1 < 8:
     print("Ошибка: в пароле меньше 8 символов")
 else:
     result=result+1
+if check2 != True:
+    print("Ошибка: в пароле нет заглавной буквы")
+else:
+    result=result+1
+
 
 
