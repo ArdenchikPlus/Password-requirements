d03994ddc43bde7069808660836545a14da14e4a
ArdenchikPlus <raskovalov132@gmail.com>
2026-06-04 21:18:47 +0300

    last version

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ n=input()
 check1=0
 check2=False
 check3=False
+check4=False
 result=0
 num="123456789"
+st="!@#$%"
 for i in range(len(n)):
     check1=check1+1
     if n[i].isupper():
         check2=True
     if n[i] in num:
         check3=True
+    if n[i] in st:
+        check4=True
 if check1 < 8:
     print("Ошибка: в пароле меньше 8 символов")
 else:
@@ -22,5 +26,9 @@ if check3 == False:
     print("Ошибка: в пароле нет цифр")
 else:
     result=result+1
-
-
+if check4 == False:
+    print("Ошибка: в пароле нет символов")
+else:
+    result=result+1
+if result == 4:
+    print("Ваш пароль надёжный")
